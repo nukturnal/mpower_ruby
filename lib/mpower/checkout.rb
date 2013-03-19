4fad4a78b675c1665db078d7682741d3feb6efdd
Alfred Robert Rowe <alfred@ncodedevlabs.com>
2013-03-19 11:19:06 +0000

    Code Cleanup

--- lib/mpower/checkout.rb
+++ lib/mpower/checkout.rb
@@ -3,6 +3,15 @@ module MPower
     class Core
       include MPower::Utilities
       attr_accessor :status, :response_text, :response_code, :transaction_id, :description, :token
+
+      def push_results(result={})
+        @transaction_id = result["transaction_id"]
+        @description = result["description"]
+        @response_code = result["response_code"]
+        @response_text = result["response_text"]
+        @token = result["token"]
+        @response_code == "00" ? @status = MPower::SUCCESS : @status = MPower::FAIL
+      end
     end
   end
 end
--- lib/mpower/direct_pay.rb
+++ lib/mpower/direct_pay.rb
@@ -9,16 +9,10 @@ module MPower
 
       result = http_json_request(MPower::Setup.direct_pay_credit_base_url,payload)
       if result["response_code"] == "00"
-        @transaction_id = result["transaction_id"]
-        @description = result["description"]
-        @response_code = result["response_code"]
-        @response_text = result["response_text"]
-        @status = MPower::SUCCESS
+        push_results(result)
         true
       else
-        @response_text = result["response_text"]
-        @response_code = result["response_code"]
-        @status = MPower::FAIL
+        push_results(result)
         false
       end
     end
