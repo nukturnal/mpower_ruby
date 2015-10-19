d186f1f76adc332f9b16d3d1bfe77d38979b665c
Alfred Rowe <alfred@encodevlabs.com>
2015-10-19 09:25:06 +0000

    Simplified Codebase

--- lib/mpower/direct_card.rb
+++ lib/mpower/direct_card.rb
@@ -3,16 +3,8 @@ module MPower
     attr_accessor :unity_transaction_id
 
     def charge(amount,card_details={})
-      payload = {
-        :card_name => card_details[:card_name],
-        :card_number => card_details[:card_number],
-        :card_cvc => card_details[:card_cvc],
-        :exp_month => card_details[:exp_month],
-        :exp_year => card_details[:exp_year],
-        :amount => amount
-      }
-
-      result = http_json_request(MPower::Setup.direct_card_charge_base_url,payload)
+      card_details[:amount] = amount
+      result = http_json_request(MPower::Setup.direct_card_charge_base_url,card_details)
 
       if result["response_code"] == "00"
         push_results(result)
